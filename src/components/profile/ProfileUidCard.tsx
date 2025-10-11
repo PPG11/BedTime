4078e16fb2eb58fa5495fa0c0539632bcdc08688
tim <luolie0577@gmail.com>
2025-10-11 18:44:19 +0800

    feat: update some button style

--- src/components/profile/ProfileUidCard.tsx
+++ src/components/profile/ProfileUidCard.tsx
@@ -11,7 +11,7 @@ export function ProfileUidCard({ uid, onCopy }: ProfileUidCardProps) {
       <Text className='profile__title'>我的账号</Text>
       <Text className='profile__uid'>{uid || '正在生成 UID...'}</Text>
       <Text className='profile__uid-hint'>将 UID 分享给好友，他们就能关注你的早睡打卡啦。</Text>
-      <Button className='profile__copy' size='mini' onClick={onCopy}>
+      <Button className='profile__copy' onClick={onCopy}>
         复制 UID
       </Button>
     </View>
